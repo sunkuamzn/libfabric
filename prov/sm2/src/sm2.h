708d1e075f4e68095accb0b41274eda6e4d98055
Sai Sunku <sunkusa@amazon.com>
2023-05-10 21:41:55 +0000

    prov/sm2: Enable HMEM IPC in SM2
    This commit introduces HMEM IPC capabilities to SM2. IPC protocol allows
    transfers of arbitrary sized messages from/to device memory.
    
    This commit also uses gdrcopy for small message sizes on CUDA devices as
    an optimization
    
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>

--- prov/sm2/src/sm2.h
+++ prov/sm2/src/sm2.h
@@ -92,6 +92,7 @@ extern pthread_mutex_t sm2_ep_list_lock;
 enum {
 	sm2_proto_inject,
 	sm2_proto_cma,
+	sm2_proto_ipc,
 	sm2_proto_max,
 };
 
@@ -238,6 +239,7 @@ struct sm2_xfer_ctx {
 
 struct sm2_domain {
 	struct util_domain util_domain;
+	struct ofi_mr_cache *ipc_cache;
 	struct fid_peer_srx *srx;
 };
 
@@ -324,6 +326,7 @@ static inline bool sm2_proto_imm_send_comp(uint16_t proto)
 {
 	switch (proto) {
 	case sm2_proto_cma:
+	case sm2_proto_ipc:
 		return false;
 	default:
 		return true;
