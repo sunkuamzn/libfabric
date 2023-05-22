8cde284221c783e1c9455b74495ed53f1f4880a6
Sai Sunku <sunkusa@amazon.com>
2023-05-22 04:54:48 +0000

    prov/sm2: Add host to device IPC protocol
    This protocol enables the CMA protocol to send arbitrary sized messges
    from host memory to device memory
    
    The sender attempts to send the message using the CMA protocol. But when
    the receiver realizes that the destination in on a device memory, the
    receiver opens an IPC handle and sends it back to the sender. The sender
    then performs a device memcpy into the destination memory.
    
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>

--- prov/sm2/src/sm2.h
+++ prov/sm2/src/sm2.h
@@ -115,6 +115,10 @@ sender will generate the completion entry when it receives such an xfer_entry.
  * send completion. */
 #define SM2_GENERATE_COMPLETION (1 << 2)
 
+/* Protocol flags for SM2 CMA protocol */
+#define SM2_CMA_HOST_TO_DEV	(1 << 3)
+#define SM2_CMA_HOST_TO_DEV_ACK (1 << 4)
+
 /*
  * 	next - fifo linked list next ptr
  * 		This is volatile for a reason, many things touch this
@@ -176,6 +180,10 @@ struct sm2_atomic_entry {
 struct sm2_cma_data {
 	size_t iov_count;
 	struct iovec iov[SM2_IOV_LIMIT];
+
+	/* Used for IPC host to device protocol */
+	struct ipc_info ipc_info;
+	struct fi_peer_rx_entry *rx_entry;
 };
 
 struct sm2_ep_name {
