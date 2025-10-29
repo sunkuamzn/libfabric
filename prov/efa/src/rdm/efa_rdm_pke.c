29f22c1f7f00f2146a8a2e0a73ae7c78379fe563
Sai Sunku <sunkusa@amazon.com>
2025-10-29 23:22:56 +0000

    prov/efa: Poison pkes and fail if a poisoned pke is processed
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>

--- prov/efa/src/rdm/efa_rdm_pke.c
+++ prov/efa/src/rdm/efa_rdm_pke.c
@@ -45,9 +45,7 @@ struct efa_rdm_pke *efa_rdm_pke_alloc(struct efa_rdm_ep *ep,
 	if (!pkt_entry)
 		return NULL;
 
-#ifdef ENABLE_EFA_POISONING
 	efa_rdm_poison_mem_region(pkt_entry, pkt_pool->attr.size);
-#endif
 	dlist_init(&pkt_entry->entry);
 
 #if ENABLE_DEBUG
--- prov/efa/src/rdm/efa_rdm_util.h
+++ prov/efa/src/rdm/efa_rdm_util.h
@@ -23,14 +23,12 @@ int efa_rdm_construct_msg_with_local_and_peer_information(struct efa_rdm_ep *ep,
 
 int efa_rdm_write_error_msg(struct efa_rdm_ep *ep, struct efa_rdm_peer *peer, int prov_errno, char *err_msg, size_t *buflen);
 
-#ifdef ENABLE_EFA_POISONING
 static inline void efa_rdm_poison_mem_region(void *ptr, size_t size)
 {
 	uint32_t efa_rdm_poison_value = 0xdeadbeef;
 	for (int i = 0; i < size / sizeof(efa_rdm_poison_value); i++)
 		memcpy((uint32_t *)ptr + i, &efa_rdm_poison_value, sizeof(efa_rdm_poison_value));
 }
-#endif
 
 
 #endif /* _EFA_RDM_UTIL_H */
